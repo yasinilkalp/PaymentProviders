3cb23c5ed6eab0b5e103df3de0b90288c1b5b76a
Yasin ilkalp Arabacı <yasinilkalp_24@hotmail.com>
2022-11-02 11:43:41 +0300

    TotalAmount set double to decimal

--- Models/PaymentRequest.cs
+++ Models/PaymentRequest.cs
@@ -14,7 +14,7 @@ namespace PaymentProviders.Models
         public int ExpireYear { get; set; }
         public string CvvCode { get; set; }
         public int Installment { get; set; }
-        public double TotalAmount { get; set; }
+        public decimal TotalAmount { get; set; }
         public string OrderNumber { get; set; }
         public string CurrencyIsoCode { get; set; }
         public string LanguageIsoCode { get; set; }
